802e65bdc9d9ca6d362bcd4699f48b6480552c74
Eduardo De La Cruz <edelacruz@teravisiontech.com>
2017-11-27 17:25:58 -0400

    Finalizado

--- Programacion Basica/ViewController.m
+++ Programacion Basica/ViewController.m
@@ -18,6 +18,25 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    int x, y;
+    
+    x = 5;
+    y = -3;
+    
+    int radioDeLaTierra = 6730;
+    int radioDeLaTierra2 = 2 * radioDeLaTierra;
+    
+    /*z = x * y;
+    z = x + y;
+    z = x - y;*/
+    
+    int z = (x / y);
+    NSLog(@"Mi valor de z es: %d", z);
+    float zFloat = ((1.0 * x) / (1.0 * y));
+    NSLog(@"Mi valor de z como float es: %f", zFloat);
+    double zDouble = ((1.0 * x) / (1.0 * y));
+    NSLog(@"Mi valor de z como double es: %f", zDouble);
 }
 
 - (void)didReceiveMemoryWarning
